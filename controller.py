b42c885930e81bb980757d8c39c8c6ee148708e6
Andrei <fmwar92@gmail.com>
2022-10-02 09:20:48 +0300

    Job in progress

--- controller.py
+++ controller.py
@@ -1,24 +1,26 @@
 import functional
 
 def choice_todo():
-    print("""Здравствуйте!
-    Выберите действие:
-    1 - Добавить контакт
-    2 - Показать контакты
-    3 - Поиск контакта.""")
-    ch = input("Введите цифру: ")
-    if ch == '1':
-        functional.import_data(functional.input_data())
-    elif ch == '2':
-        data = functional.export_data()
-        functional.print_data(data)
-    else:
-        word = input("Введите данные для поиска: ")
-        data = functional.export_data()
-        item = functional.search_data(word, data)
-        if item != None:
-            print("Фамилия".center(20), "Имя".center(20), "Телефон".center(15), "Примечание".center(30))
-            print("-"*85)
-            print(item[0].center(20), item[1].center(20), item[2].center(15), item[3].center(30))
+    print('Добро пожаловать!')
+    while True:
+        print("""Выберите действие:
+        1 - Добавить контакт
+        2 - Показать контакты
+        3 - Поиск контакта.
+        0 - Выйти из справочника.""")
+        ch = input("Введите цифру: ")
+        if ch == '1':
+            functional.import_data(functional.input_data())
+        elif ch == '2':
+            data = functional.export_data()
+            functional.print_data(data)
+        elif ch == '3':
+            word = input("Введите данные для поиска: ")
+            data = functional.export_data()
+            item = functional.search_data(word, data)
+            if item != None:
+                print("Фамилия".center(20), "Имя".center(20), "Телефон".center(15), "Примечание".center(30))
+                print("-"*85)
+                print(item[0].center(20), item[1].center(20), item[2].center(15), item[3].center(30))
         else:
-            print("Данные не обнаружены")
+            break
